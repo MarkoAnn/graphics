1958a74f76437c2a7827ff8bb8e2e8265cbd07cb
hi <hi@KOMPUTER>
2024-03-16 11:04:16 +0200

    Добавьте файлы проекта.

---
+++ graphics/graphics.cpp
@@ -0,0 +1,11 @@
+﻿#include "graphics.h"
+#pragma comment(lib,"graphics.lib")
+int main()
+{
+    initwindow(500, 500);
+    line(100, 100, 300, 300);
+
+    getch();
+    closegraph();
+    return 0;
+}
