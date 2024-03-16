b03368d964cd5a273a399f14fda4b39009ae7664
hi <hi@KOMPUTER>
2024-03-16 11:44:36 +0200

    треугольник

--- graphics/graphics.cpp
+++ graphics/graphics.cpp
@@ -1,16 +1,33 @@
-﻿#include "graphics.h"
+﻿#include <iostream>
+#include "graphics.h"
 #pragma comment(lib,"graphics.lib")
+
+void drawTriangle(int x1, int y1, int x2, int y2, int x3, int y3) {
+    line(x1, y1, x2, y2);
+    line(x2, y2, x3, y3);
+    line(x3, y3, x1, y1);
+}
+
 
 int main() {
     initwindow(500, 500);
+    int x1;
+    int y1;
+    int x2;
+    int y2; 
+    int x3;
+    int y3;
+    std::cout << "введите точки треугольника: ";
+    std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
 
-    // Рисуем линию
-    line(100, 100, 300, 300);
-    // Ждем две секунды
+    drawTriangle(x1, y1, x2, y2, x3, y3);
+
+
+    /*
     delay(2000);
-    // Очищаем экран и устанавливаем фон черным
     cleardevice();
     setbkcolor(BLACK);
+    */
 
     getch();
     closegraph();
