a4089a18286726560d8dc75405d0110b8318ea0c
hi <hi@KOMPUTER>
2024-03-16 11:16:41 +0200

    ожидание

--- graphics/graphics.cpp
+++ graphics/graphics.cpp
@@ -1,9 +1,16 @@
 ﻿#include "graphics.h"
 #pragma comment(lib,"graphics.lib")
-int main()
-{
+
+int main() {
     initwindow(500, 500);
+
+    // Рисуем линию
     line(100, 100, 300, 300);
+    // Ждем две секунды
+    delay(2000);
+    // Очищаем экран и устанавливаем фон черным
+    cleardevice();
+    setbkcolor(BLACK);
 
     getch();
     closegraph();
